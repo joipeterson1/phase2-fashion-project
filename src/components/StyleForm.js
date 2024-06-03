a162c96456bf8d7f4bcbfbc75f5d277eaa553f1f
Dalecia <petersondalecia@gmail.com>
2024-06-03 13:05:29 -0400

    functional form and post request

--- src/components/StyleForm.js
+++ src/components/StyleForm.js
@@ -1,41 +1,46 @@
 import {useOutletContext} from "react-router-dom"
-import {useState} from "react"
+
 
 function StyleForm(){
  const formImg= "https://www.careergirls.org/wp-content/uploads/2018/05/FashionDesigner_1920x1080.jpg"
-const [top, setTop]=useState('')
-const [bottom, setBottom]=useState('')
-const [shoe, setShoe]=useState('')
-const [accessories, setAccessories]=useState('')
-const [imageURL, setImageURL]=useState('')
-const styles = useOutletContext()
+const {handleSubmit,
+    top, 
+    setTop,
+    bottom, 
+    setBottom,
+    shoes, 
+    setShoes,
+    accessories, 
+    setAccessories,
+    image,
+    setImage}= useOutletContext()
 
 
     return(
         <div>
             <h3> Add a New Style to Your Portfolio!!!</h3>
             <img alt='' src={formImg} />
-            <form action="/my-handling-form-page" method="post">
+            <form onSubmit={handleSubmit}>
                 <ul className="form-ul">
                      <li>
                         <label>Top Decription:</label>
-                        <input type="text" value={top} onChange={(e)=> setTop(e.target.value)}/>
+                        <input type="text" value={top} onChange={((e)=> setTop(e.target.value))}/>
                     </li>
                      <li>
                         <label>Bottom Decription:</label>
-                        <input type="text" value={bottom} onChange={(e)=> setBottom(e.target.value)}/>
+                        <input type="text" value={bottom} onChange={((e)=> setBottom(e.target.value))}/>
                     </li>
                     <li>
                          <label>Shoe Decription:</label>
-                         <input type="text" value={shoe} onChange={(e)=> setShoe(e.target.value)}/>
+                         <input type="text" value={shoes} onChange={((e)=> setShoes(e.target.value))}/>
                     </li>
                     <li>
-                         <label>Accessories Decription:</label>
-                         <input type="text" value={accessories} onChange={(e)=> setAccessories(e.target.value)}/>
+                         <label>Accessories Decription:</label> 
+                         <input type="text" value={accessories} onChange={((e)=> setAccessories(e.target.value))}/>
                     </li>
                     <li>
                          <label>Image URL:</label>
-                         <input type="text" value={imageURL} onChange={(e)=> setImageURL(e.target.value)}/>
+                         <input type="text" value={image} onChange={((e)=> setImage(e.target.value))}/>
                     </li>
                     <li className="button">
                     <button type="submit">Add Style!</button>
--- src/components/StyleList.js
+++ src/components/StyleList.js
@@ -4,7 +4,36 @@ import {useEffect, useState} from "react"
 import {Link, Outlet} from "react-router-dom"
 
 function StyleList(){
-    const [styles, setStyles]= useState([])
+const [styles, setStyles]= useState([])
+const [top, setTop]=useState('')
+const [bottom, setBottom]=useState('')
+const [shoes, setShoes]=useState('')
+const [accessories, setAccessories]=useState('')
+const [image, setImage]=useState('')
+
+
+function handleSubmit(e){
+    e.preventDefault()
+    const submittedData = {
+        id: `${styles.length + 1}`,
+        top: top,
+        bottom: bottom,
+        shoes: shoes,
+        accessories: accessories,
+        image: image,
+      };
+
+    fetch('http://localhost:3000/styles', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(submittedData)
+    })
+    .then((r)=> r.json())
+    .then((newData)=> setStyles(newData))
+
+}
 
     useEffect(()=> {
       fetch("http://localhost:3000/styles")
@@ -27,7 +56,19 @@ const styleDisplay = styles.map((style)=> (
                 <button>
                 <Link to={"/style-list/style-form"} styles={styles}>Add a New Style!</Link>
                 </button>
-                {<Outlet context={styles}/>}
+                {<Outlet context={{
+                    handleSubmit: handleSubmit,
+                    top: top, 
+                    setTop: setTop,
+                    bottom: bottom, 
+                    setBottom: setBottom,
+                    shoes: shoes, 
+                    setShoes: setShoes,
+                    accessories: accessories, 
+                    setAccessories: setAccessories,
+                    image: image,
+                    setImage: setImage
+                }}/>}
             </div>
         )
     }
