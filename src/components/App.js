577212465f1a8d26e2c1a1c2de279fecf609f4af
Dalecia <petersondalecia@gmail.com>
2024-06-19 07:49:26 -0400

    detail toggle

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,7 @@ function App() {
     setStyles([...styles, submittedData])
 }
 
+
   return (
     <div>
       <header>
--- src/components/StyleCard.js
+++ src/components/StyleCard.js
@@ -1,14 +1,33 @@
+import {useState} from "react"
 
 function StyleCard({style}){
+const details = 
+    <div>
+    <h3>Style #{`${style.id}`} Description</h3>
+    <p>Top: {style.top} </p>
+    <p>Bottoms: {style.bottom}</p>
+    <p>Shoes: {style.shoes}</p>
+    <p>Accessories: {style.accessories}</p>   
+    </div>
+const [showDetails, setShowDetails] = useState(false)
+const [detailDisplay, setDetailDisplay] = useState('')
+
+
+function handleDetailClick(){
+    if (showDetails === true){
+        setDetailDisplay(details)
+    } else {
+        setDetailDisplay('')
+    }
+    setShowDetails((showDetails)=> !showDetails)
+}
+
 
     return(
         <div>
             <img alt="" src={style.image} />
-            <h3>Style #{`${style.id}`} Description</h3>
-            <p>Top: {style.top} </p>
-            <p>Bottoms: {style.bottom}</p>
-            <p>Shoes: {style.shoes}</p>
-            <p>Accessories: {style.accessories}</p>   
+            <button onClick={handleDetailClick}>Details</button>  
+            {detailDisplay}
         </div>
     )
 }
